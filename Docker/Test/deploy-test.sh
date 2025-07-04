ba59580b5844e96f8067f6034ec3b90b49b8eedc
Haroun Ahmad <HarounAhmad02@gmail.com>
2025-07-04 13:01:05 +0200

    update docker

--- Docker/Test/deploy-test.sh
+++ Docker/Test/deploy-test.sh
@@ -3,8 +3,13 @@
 
 docker build -t vpn-test-server .
 
-docker run --privileged --cgroupns=host \
+docker run \
+  --privileged --cgroupns=host \
+  --name vpn-server-debug \
   --cap-add=NET_ADMIN \
   --device /dev/net/tun \
-  -d \
+  -p 1194:1194/udp \
+  -p 6443:6443/tcp \
+  -v ~/vpn-test:/etc/openvpn \
+  -v ~/k3s-data:/var/lib/rancher/k3s \
   vpn-test-server
--- Docker/Test/entrypoint.sh
+++ Docker/Test/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Starting k3s server in foreground..."
-exec /usr/local/bin/k3s server --tls-san 127.0.0.1
+echo "Starting k3s..."
+/usr/local/bin/k3s server --tls-san 127.0.0.1 > /var/log/k3s.log 2>&1 &
+
+echo "Starting OpenVPN..."
+ls /etc/openvpn/
+cd /etc/openvpn/
+pwd
+openvpn --config /etc/openvpn/server.conf > /var/log/openvpn.log 2>&1 &
+
+echo "Processes running in background. Use logs:"
+echo "  tail -f /var/log/k3s.log"
+echo "  tail -f /var/log/openvpn.log"
+
+exec tail -F /var/log/k3s.log /var/log/openvpn.log
+
+
+# Wait for both to exit
+wait -n
